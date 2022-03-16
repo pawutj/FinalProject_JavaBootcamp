dfe14d6e9eeeb5d95e1e2097d16b3b46dce44804
astralaria <pawut.j@gmail.com>
2022-03-17 04:36:00 +0700

    Create: Init Project

---
+++ src/main/java/finalproject/cashtracking/CashtrackingApplication.java
@@ -0,0 +1,13 @@
+package finalproject.cashtracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashtrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashtrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/finalproject/cashtracking/CashtrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package finalproject.cashtracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashtrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
